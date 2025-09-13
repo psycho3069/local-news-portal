2ce8708418fb7d2fd72d00bdc23d2106e07fc0fe
Farhan Khan <farha100669@gmail.com>
2025-09-13 19:41:54 +0600

    Initial commit

---
+++ asset/js/custom.js
@@ -0,0 +1,15 @@
+/*************************************
+
+Template Name: News Narayanganj - Online NewsPaper
+Author: Jonayed Islam
+Author Url: http://jonayed.com
+Version: 1.0
+Design and Developed by: Bongosoft Ltd.
+
+****************************************/
+
+(function ($) {
+  "use strict";
+  var $window = $(window),
+    $body = $("body");
+})(jQuery);
